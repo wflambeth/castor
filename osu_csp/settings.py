8d330d6b3a1adabef8792dbce0dc15c18d2a3dad
will_lambeth <wflambeth@gmail.com>
2022-11-13 13:49:26 -0800

    Confirm Postgres connection, set up admin view

--- osu_csp/settings.py
+++ osu_csp/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "planner.apps.PlannerConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class User(models.Model):
+    email = models.CharField(max_length=200)
+    signup_date = models.DateTimeField('signup date')
+
+    def __str__(self):
+        return self.email
+
+    def signed_up_recently(self):
+        return self.signup_date >= timezone.now() - datetime.timedelta(days=1)
+
+"""
+class Course(models.Model):
+    pass
+
+class Schedule(models.Model):
+    pass
+
+class Class_to_Schedule(models.Model):
+    pass
+
+class Prereq(models.Model):
+    pass
+"""
