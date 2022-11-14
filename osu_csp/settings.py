1bb24b98106d23866f565037818dae5e1495d474
will_lambeth <wflambeth@gmail.com>
2022-11-13 16:09:23 -0800

    Create core models/fields for DB

--- osu_csp/settings.py
+++ osu_csp/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Overwrote default Django user model w/ extensible custom
+AUTH_USER_MODEL = 'planner.User'
--- planner/models.py
+++ planner/models.py
@@ -1,28 +1,47 @@
-import datetime
-
 from django.db import models
-from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-class User(models.Model):
-    email = models.CharField(max_length=200)
-    signup_date = models.DateTimeField('signup date')
-
-    def __str__(self):
-        return self.email
-
-    def signed_up_recently(self):
-        return self.signup_date >= timezone.now() - datetime.timedelta(days=1)
-
-"""
-class Course(models.Model):
+class User(AbstractUser):
     pass
+
+class Course(models.Model):
+    # using non-serial PK for this, we'll see how it goes
+    course_number = models.PositiveSmallIntegerField(primary_key=True)
+    title = models.CharField(max_length=100, unique=True)
+    credits = models.PositiveSmallIntegerField(
+        default=4,
+        validators=[MaxValueValidator(16), MinValueValidator(1)]
+    )
+    required = models.BooleanField()
 
 class Schedule(models.Model):
-    pass
+    class Quarter(models.IntegerChoices):
+        WINTER = 0
+        SPRING = 1
+        SUMMER = 2
+        FALL = 3
 
-class Class_to_Schedule(models.Model):
-    pass
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, default=f"{user.name}'s Schedule{str(id)}") # not enforcing uniqueness on this for now
+    start_qtr = models.PositiveSmallIntegerField(choices=Quarter.choices)
+    end_qtr = models.PositiveSmallIntegerField(choices=Quarter.choices)
+    start_year = models.PositiveSmallIntegerField() 
+    end_year = models.PositiveSmallIntegerField()
+
+    # TODO: add check constraint for start < end
+
+class Course_Schedule(models.Model):
+    schedule = models.ForeignKey('Schedule', on_delete=models.CASCADE)
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    year = models.PositiveSmallIntegerField()
+    qtr = models.PositiveSmallIntegerField(choices=Schedule.Quarter.choices)
+
+    constraints = [
+        models.UniqueConstraint(fields=['schedule', 'course'], name='unique_class_instance_per_schedule')
+        # TODO: add check constraint for year/qtr within schedule bounds
+    ]
 
 class Prereq(models.Model):
-    pass
-"""
+    course = models.ForeignKey('Course', related_name='course', on_delete=models.CASCADE)
+    prereq = models.ForeignKey('Course', related_name='prereq', on_delete=models.CASCADE)
