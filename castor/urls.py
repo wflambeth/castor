89279a0df6931ffedc625198fcdf66e2e46bf5d1
will_lambeth <wflambeth@gmail.com>
2023-01-06 14:01:49 -0800

    Remove app prefix for planner URL paths

--- castor/urls.py
+++ castor/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('csp/', include('planner.urls')),
+    path('', include('planner.urls')),
     path("admin/", admin.site.urls),
 ]
