5e0fc87b95c6edf3947463bf8c98aafde5fa9904
will_lambeth <wflambeth@gmail.com>
2023-01-05 19:57:04 -0800

    Complete project rename

--- castor/urls.py
+++ castor/urls.py
@@ -1,4 +1,4 @@
-"""osu_csp URL Configuration
+"""castor URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
