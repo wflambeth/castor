6c4e48c3424ce47c7e4d2732c9b73c608246dc8d
will_lambeth <wflambeth@gmail.com>
2023-01-06 13:12:43 -0800

    Remove secret from version control

--- castor/settings.py
+++ castor/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^s8kuglij^7+maqbtl1dvw=6=53bd*$vm&(c$9y$vcjh*u*r*h"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
