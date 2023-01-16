90bbb7a713047b928535be5812dd0f4ba4a67ba0
will_lambeth <wflambeth@gmail.com>
2023-01-16 11:50:32 -0800

    Modify URL

--- castor/urls.py
+++ castor/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', include('planner.urls')),
-    path("admin/", admin.site.urls),
+    path("sG8muBDox7No4g/", admin.site.urls),
     path("accounts/", include('allauth.urls')),
     path("accounts/", RedirectView.as_view(pattern_name='account_login'), name='accounts-redirect'),
     path("accounts/profile/", RedirectView.as_view(pattern_name='index'), name='accounts-redirect')
