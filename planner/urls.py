bffcbe372ffcea9f23399e445efa99ca1e052355
will_lambeth <wflambeth@gmail.com>
2023-04-27 15:13:31 -0700

    Remove unused create() view

--- planner/urls.py
+++ planner/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('schedule/<int:sched_id>', views.router, name='router'),
-    path('create', views.create, name='create'),
     path('save', views.save, name='save'),
     path('update_title', views.update_title, name='update_title')
 ]
