7f695336b805288afd51a82648b05e13a6c0beda
will_lambeth <wflambeth@gmail.com>
2023-07-08 18:10:01 -0700

    Add test database fixture, add tests

--- planner/tests.py
+++ planner/tests.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
-import requests
+import requests, datetime
 
 
 class TestStaticAssets(LiveServerTestCase):
@@ -36,23 +36,36 @@ class TestStaticAssets(LiveServerTestCase):
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, 200, f'Failed to load {url}: {response.status_code}')
 
-class HomePageContentTest(LiveServerTestCase):
+class TestHomePageDemo(LiveServerTestCase):
     # currently just testing static page contents - might do separate class for drag and drop tests
+
+    fixtures = ['latest-test-data.json']
 
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.get(self.live_server_url)
-        self.browser.implicitly_wait(2) # implicit wait = wait this long to give up on finding element
+        self.browser.implicitly_wait(2) # implicit wait = "wait this long to give up on finding element"
+
+        # get/store demo schedule contents
 
     def tearDown(self):
         self.browser.quit()
     
     # page title is correct
-    def test_home_page_title(self):
+    def test_homepage_title(self):
         self.assertIn('C.A.S.T.O.R.', self.browser.title)
 
     # schedules list is empty
-    def test_home_page_empty_schedule_list(self):
+    def test_homepage_schedule_list_empty(self):
         schedules_list = self.browser.find_element(By.ID, 'schedules-list')
         self.assertEqual('', schedules_list.text)
     
+    # schedule has correct quarters (all four of current year)
+    def test_homepage_schedule_quarters(self):
+        demo_qtrs = self.browser.find_elements(By.CLASS_NAME, 'qtr')
+        qtr_vals = [qtr.get_attribute('data-qtr') for qtr in demo_qtrs]
+        qtr_vals.sort()
+        self.assertEqual(qtr_vals, ['0', '1', '2', '3'])
+        for qtr in demo_qtrs:
+            self.assertEqual(qtr.get_attribute('data-yr'), str(datetime.datetime.now().year))
+
